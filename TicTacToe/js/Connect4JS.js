dd28f81d28aaec10333b5bd4c3b9d8d8d54b1678
Arthur Soto <arsoto@csumb.edu>
2022-12-06 19:54:42 -0800

    tictac

--- TicTacToe/js/Connect4JS.js
+++ TicTacToe/js/Connect4JS.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-let grid = { t1:" ", t2: " ", t3: " ", m1: " ", m2: " ", m3: " ", b1: " ", b2: " ", b3: " "}
+// let grid = { t1:" ", t2: " ", t3: " ", m1: " ", m2: " ", m3: " ", b1: " ", b2: " ", b3: " "}
+//
+// grid.m2 = "X";
 
-grid.m2 = "X";
+console.log("The code works, you can go ahead and play if there are no errors!");
 
 
-console.log("The code works");
 
+// This portion of code makes it so that whatver box you click on, it will cycle between an X, O or blank symbol
 let clickBox = (e) => {
 	console.log("Changed whatever box you clicked on");
     // e.currentTarget.textContent = 'X';
@@ -18,7 +20,21 @@ let clickBox = (e) => {
             e.currentTarget.textContent = 'X';
         }
 }
+// This closes out the fucntion above
 
+
+
+// let playerTurn = (e) =>{
+// 		if its player 1's turn
+// 			let e.currentTarget = 'X'
+// 		else if its player 2's turn
+// 				let e.currentTarget = 'O'
+// }
+
+
+
+
+// These repeating strings of code target the box that's clicked to make the symbol change
 let changeBox = document.querySelector("#box1")
 changeBox.addEventListener('click', clickBox)
 
@@ -45,4 +61,30 @@ changeBox8.addEventListener('click', clickBox)
 
 let changeBox9 = document.querySelector("#box9")
 changeBox9.addEventListener('click', clickBox)
+// This closes out the code above
+
+
+
+//This section of code is meant to display and check for a win
+
+//let winStates = [[box1, box2, box3], [box4, box5, box6], [box7, box8, box9], [box1, box4, box7], [box 2, box5, box8], [box3, box6, box9], [box1, box5, box 9], [box3, box 5, box7]]
+
+// This code should make it so that when the first row is all == x it auto wins the game and makes a alert appear
+	//let winStates = if (box1.textContent === 'X' && box2.textContent === 'X' && box3.textContent === 'X') {
+	//alert(X wins the game!)
+	//}
+
+
+
+// REMEMBER TO MAKE THIS INTO A doc.querySelectorAll FUNCTION
+//This function works for only the the first box right now
+let resetGameFunction = () => {
+    console.log("You reset the game, go ahead and play again!")
+	document.querySelector(".aBox").textContent = ' ';
+}
+
+//USE FOR EACH IN HERE BEFORE SUBMITTING FINAL ASSIGNMENT
+let restartGame = document.querySelector(".resetGame")
+  restartGame.addEventListener('click', resetGameFunction)
+
 });
---
+++ lab13/js/lab13.js
@@ -0,0 +1,4 @@
+function hoverName() {
+ 	document.body.style.cursor = 'pointer';
+ 	document.style.fontWeight = 'bold';"
+}
